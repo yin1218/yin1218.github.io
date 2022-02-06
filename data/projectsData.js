163ec29698ec46e28d0020681778c4f4f0ac4b67
WU,CHIEN YIN <wcy881218@gmail.com>
2022-02-06 16:48:09 +0800

    minor change on name

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,25 +2,25 @@ const projectsData = [
   {
     title: 'Pathfinder',
     description: `A mobile application helping students to write their academic portfolios in an efficient way and analyzed suitable majors based on their records`,
-    imgSrc: '/static/images/projects/pathfinder.png',
+    imgSrc: '/static/images/project/pathfinder.png',
     href: 'https://www.youtube.com/watch?v=AEFjUuhPILk',
   },
   {
     title: 'GOTCHA',
     description: `GOTCHA, got you in charity! This is a mobile application prototype that helped reduce uneven fundraising of small nonprofit organizations`,
-    imgSrc: '/static/images/projects/gotcha.png',
+    imgSrc: '/static/images/project/gotcha.png',
     href: 'https://www.youtube.com/watch?v=YXzMDNRGixM',
   },
   {
     title: 'Dormy, be your homie',
     description: `A website helped students live in dorms solve their daily needs`,
-    imgSrc: '/static/images/projects/dormy.png',
+    imgSrc: '/static/images/project/dormy.png',
     href: 'https://youtu.be/gYOxBSz9LmE',
   },
   {
     title: 'Swappy',
     description: `A mobile application aimed to encourage people to exchange their unused items by “Decluttering challenge” and “barter-based group” functions`,
-    imgSrc: '/static/images/projects/swappy.png',
+    imgSrc: '/static/images/project/swappy.png',
     href: 'https://www.youtube.com/watch?v=HI4xZkTqql0',
   },
   // {
